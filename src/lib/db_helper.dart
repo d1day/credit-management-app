2120da95f48757467bcbbdeb1723f0ea85716458
d1day <daichi.arai24@gmail.com>
2024-02-18 22:40:37 +0900

    db_table

--- src/lib/db_helper.dart
+++ src/lib/db_helper.dart
@@ -1,15 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class DatabaseHelper {
+  static late Database database;
   static late DatabaseFactory databaseFactory;
 
   static Future<Database> initializeDatabase() async {
     databaseFactory = databaseFactoryFfi;
-    final database = await openDatabase(
-      join(await getDatabasesPath(), 'my_database.db'),
+    final dbDirectory = await getApplicationSupportDirectory();
+    final dbFilePath = dbDirectory.path;
+    final path = join(dbFilePath, 'my_database.db');
+
+    database = await openDatabase(
+      path,
+      version: 1,
       // 外部キー制約を有効にする
       onConfigure: (db) async {
         await db.execute('PRAGMA foreign_keys = ON;');
@@ -36,27 +43,26 @@ class DatabaseHelper {
             NM_DAY TEXT,
             N_PERIOD INTEGER,
             ID_LECTURE INTEGER,
-            FOREIGN KEY (ID_LECTURE) references lecturetable(ID_LECTURE))
+            FOREIGN KEY (ID_LECTURE) references lecturetable(ID_LECTURE)
           )
         ''');
         await db.execute('''
           CREATE TABLE M_DIV_LECTURE(
             CD_DIV_LECTURTE TEXT,
             NM_DIV_LECTURE TEXT,
-            ID_LECTURE INTEGER,
+            ID_LECTURE INTEGER
           )
         ''');
         await db.execute('''
           CREATE TABLE t_attendance(
             ID_LECTURE INTEGER,
-            n_attendance INTEGER,
-            n_absence INTEGER,
-            n_behind INTEGER,
-            n_official_absence INTEGER,
+            N_ATTENDANCE INTEGER,
+            N_ABSENCE INTEGER,
+            N_BEHIND INTEGER,
+            N_OFFICIAL_ABSENCE INTEGER
           )
         ''');
       },
-      version: 1,
     );
     return database;
   }
--- src/lib/validators.dart
+++ src/lib/validators.dart
@@ -4,5 +4,7 @@ final nameValidator = MultiValidator([
   // 必須入力チェック
   RequiredValidator(errorText: "授業名は必須です"),
   // 最大長チェック
-  MaxLengthValidator(5, errorText: "名前は5文字以内で入力してください")
+  MaxLengthValidator(20, errorText: "20文字以内で入力してください")
 ]);
+
+//単位数1以上
